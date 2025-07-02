467087298004dc9960f88bc24bf0c3ef79ec169e
DinuriN <dinurin730@gmail.com>
2025-07-02 15:16:11 +0530

    initial commit

---
+++ src/sections/Navbar.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import {motion} from 'motion/react';
+
+function Navigation(){
+    return <ul className='nav-ul'>
+        <li className='nav-li'>
+            <a className='nav-link' href='#home'>Home</a>
+        </li>
+        <li className='nav-li'>
+            <a className='nav-link' href='#about'>About</a>
+        </li>
+        <li className='nav-li'>
+            <a className='nav-link' href='#work'>Work</a>
+        </li>
+        <li className='nav-li'>
+            <a className='nav-link' href='#contact'>Contact</a>
+        </li>
+    </ul>
+}
+
+const Navbar = () => {
+    const[isOPen, setIsOpen]=useState(false);
+  return (
+    <div className='fixed inset-x-0 z-20 w-full backdrop-blur-lg bg-primary/40'>
+      <div className='mx-auto c-space max-w-7xl'>
+        <div className='flex items-center justify-between py-2 sm:py-0'>
+            <a href='/' className='text-xl font-bold transition-colors text-neutral-400 hover:text-white'>Dinuri</a>
+
+            <button onClick={()=> setIsOpen(!isOPen)} className='flex cursor-pointer text-neutral-400 hover:text-white focus:outline-none sm:hidden'>
+                <img src={isOPen? 'src/assets/close.svg': 'src/assets/menu.svg'} className='w-6 h-6'/>
+            </button>
+
+            <nav className='hidden sm:flex'>
+                <Navigation/>
+            </nav>
+        </div>
+
+      </div>
+      {isOPen &&(
+        <motion.div className='block overflow-hidden text-center sm:hidden' 
+        initial={{opacity: 0, x:-10}} 
+        animate={{opacity:1, x:0}}
+        style={{maxHeight:"100vh"}}
+        transition={{duration:1}}
+        >
+        <nav className='pb-5'>
+            <Navigation/>
+        </nav>
+      </motion.div>)}
+    </div>
+  );
+}
+
+export default Navbar;

38d4ed274d3e747c7781bf3e7af5fbdf2985f284
DinuriN <dinurin730@gmail.com>
2025-07-17 11:05:14 +0530

    about grid 4 done

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -2,6 +2,7 @@ import { rotate } from "maath/buffer";
 import Card from "../components/Card";
 import { useRef } from "react";
 import { Globe } from "../components/Globe";
+import CopyEmailButton from "../components/CopyEmailButton";
 
 const About = () => {
   const grid2Container= useRef();
@@ -87,7 +88,13 @@ const About = () => {
         </div>
 
         {/* Grid 4 */}
-        <div className="grid-special-color grid-4"></div>
+        <div className="grid-special-color grid-4">
+            <div className="flex flex-col items-center justify-center gap-4 size-full">
+                <p className="text-center headtext">Do you want to start a project together?</p>
+                <CopyEmailButton/>
+
+            </div>
+        </div>
 
         {/* Grid 5 */}
         <div className="grid-default-color grid-5"></div>

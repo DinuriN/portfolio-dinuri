c3abff32b40e14d24b246963af07878c7dde7480
DinuriN <dinurin730@gmail.com>
2025-07-19 15:15:56 +0530

    footer completed

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Projects from './sections/Projects';
 import Experiences from './sections/Experiences';
 import Testimonial from './sections/Testimonial';
 import Contact from './sections/Contact';
+import Footer from './sections/Footer';
 
 function App() {
   return (
@@ -18,7 +19,7 @@ function App() {
       <Experiences/>
       <Testimonial/>
       <Contact/>
-      {/* footer */}
+      <Footer/>
     </div>
   );
 }
